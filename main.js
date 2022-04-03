987d0d3ca5446e31b0eb7fb10664b4bf9c6d09d0
Chocopai-5k <manhbeo123vp@gmail.com>
2022-04-03 19:32:06 +0700

    slide img

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const $$ = document.querySelectorAll.bind(document);
 function musicTab() {
   const tabFilter = $$(".music-trending-filter-link");
   const listFilter = $$(".music-trending--list");
-  console.log(listFilter);
   const tabActive = $(".music-trending-filter-link.active");
 
   tabFilter.forEach((tab, index) => {
@@ -21,7 +20,6 @@ function mvTab() {
   const mvtabFilter = $$(".mv-trending-filter-link");
   const mvlistFilter = $$(".mv-trending--list");
   const mvtabActive = $(".mv-trending-filter-link.active");
-    console.log(mvtabActive, mvlistFilter, mvtabActive)
   mvtabFilter.forEach((mvtab, index) => {
     const list = mvlistFilter[index];
     mvtab.onclick = function () {
@@ -30,8 +28,32 @@ function mvTab() {
 
       this.classList.add("active");
       list.classList.add("active");
+      
     };
   });
 }
+  let slideIndex = 1;
+  showSlides(slideIndex);
+  
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+
+    slides[slideIndex-1].style.display = "block";  
+  }
+
 musicTab();
 mvTab();
